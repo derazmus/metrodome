25847958962d7694886124eade2b8c504dd4fb77
Denise Erazmus <denise.erazmus@gmail.com>
2018-10-04 18:26:58 -0500

    basic js and css files added

--- react-metronome/src/index.js
+++ react-metronome/src/index.js
@@ -6,4 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Metronome />, document.getElementById('root'));
 
-registerServiceWorker();
+// If you want your Metronome to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
