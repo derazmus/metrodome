a1e677cb64b82f583ef2faf27e324b4697fa34f6
Denise Erazmus <denise.erazmus@gmail.com>
2018-10-04 18:08:55 -0500

    basic file setup

---
+++ react-metronome/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Metronome from './Metronome';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<Metronome />, document.getElementById('root'));
+
+registerServiceWorker();
